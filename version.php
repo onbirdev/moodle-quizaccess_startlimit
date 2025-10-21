b9c32c79895ad19c46a6d3bea2c463efc774d019
MohammadReza PourMohammad <mrpm.root@gmail.com>
2025-10-21 09:43:44 +0330

    update version

--- version.php
+++ version.php
@@ -27,5 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_startlimit';
-$plugin->version = 2025101900;
+$plugin->version = 2025102100;
 $plugin->requires = 2020110300;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
